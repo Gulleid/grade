fb552bc8b8423bbe380b476e9e2eaca529a04e58
Gulleid <gulleidmohamed93@gmail.com>
2023-05-25 18:07:20 -0800

    grading

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+
+const Grade=100;
+
+if(Grade>90 && Grade>=100 ){console.log("your  grade is  A ")} 
+if(Grade>90 && Grade>=75) {console.log("your grad is B ")}
+
+if(Grade>75 && Grade>=60) {console.log("your grade is C ")}
+
+if(Grade<60  ) {console.log("your grade is Failed ")}
